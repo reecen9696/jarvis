e3326d813919b4c98fe4cd48b4c0f9f863924514
reecen9696 <reece@cryptocoders.com.au>
2025-05-14 22:17:02 +1000

    ci: add GitHub Pages deploy workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // export settings for GitHub Pages
+  output: "export",
+  basePath: "/jarvis",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +12,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
